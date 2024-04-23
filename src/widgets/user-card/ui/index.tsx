f67909b45192c2577319a777d4ccffca598bacc2
Aleksandr <admin@ijo42.ru>
2024-04-23 20:00:47 +0500

    chore: avatar vk load

--- src/widgets/user-card/ui/index.tsx
+++ src/widgets/user-card/ui/index.tsx
@@ -1,6 +1,6 @@
-import React, {useMemo} from 'react';
-import {Grid, Typography, useTheme} from '@mui/material';
-import { User } from '~/entities/user/model';
+import React, {useEffect, useMemo, useState} from 'react';
+import {Box, Grid, Typography, useTheme} from '@mui/material';
+import {User} from '~/entities/user/model';
 import Block from '~/shared/ui/block';
 
 import AvatarIcon from '~/shared/assets/avatar.svg?react';
@@ -9,28 +9,55 @@ import bridge from "@vkontakte/vk-bridge";
 
 function UserCard() {
   const theme = useTheme();
+  useMemo(()=> {
+      return 5;
+  }, [])
+  const user: User = useMemo(() => ({
+      name: 'Иван',
+      lastname:
+          'Иванов',
+      patronymic:
+          'Иванович',
+      status:
+          'Студент',
+      major:
+          'ИКНТ ПМИ-1 2023',
+      description:
+          'Секретарь профбюро физического факультета, член СНО, волонтёр, член Ордена рыцарей сцены',
+  }), []);
 
-  const user: User = {
-    name: 'Иван',
-    lastname: 'Иванов',
-    patronymic: 'Иванович',
-    status: 'Студент',
-    major: 'ИКНТ ПМИ-1 2023',
-    description:
-      'Секретарь профбюро физического факультета, член СНО, волонтёр, член Ордена рыцарей сцены',
-  };
-  return (
+  const [icon, setIcon] = useState(() => (<AvatarIcon />));
+  useEffect(() => {
+    bridge.send('VKWebAppGetUserInfo').then(s => {
+      user.name = s.first_name;
+      user.lastname = s.last_name;
+      setIcon(<Box
+        component="img"
+        sx={{
+          borderRadius: '50%',
+          height: 100,
+          width: 100,
+          maxHeight: { xs: 200, md: 167 },
+          maxWidth: { xs: 200, md: 250 },
+        }}
+        src={s.photo_100}
+      />);
+    });
+  }, []);
+
+    return (
     <Block>
       <Grid container spacing={0} direction="column" alignItems="center">
         <Grid
           item
           container
-          xs={3}
+          xs={12}
           direction="row"
-
+          flexWrap="nowrap"
+          alignItems="center"
         >
             <Grid item>
-                <AvatarIcon />
+                {icon}
             </Grid>
             <Grid item xl={3} sx={{
                 marginTop: '1em',
