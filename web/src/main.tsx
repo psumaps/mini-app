257d70e606a1777d77a3336c851621bb0fe40e77
esperor <pere_strelka@mail.ru>
2024-04-29 16:03:21 +0500

    any: remove redundant code

--- web/src/main.tsx
+++ web/src/main.tsx
@@ -1,15 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './app/App.tsx';
-import { DevSupport } from '@react-buddy/ide-toolbox';
-import { ComponentPreviews, useInitial } from '~/dev';
 import bridge from '@vkontakte/vk-bridge';
 
 bridge.send('VKWebAppInit', {});
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <DevSupport ComponentPreviews={ComponentPreviews} useInitialHook={useInitial}>
-      <App />
-    </DevSupport>
+    <App />
   </React.StrictMode>,
 );
