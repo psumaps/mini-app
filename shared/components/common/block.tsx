427ac24bc22c1ba8865e92135e7c3d72cb01c574
esperor <pere_strelka@mail.ru>
2024-05-15 19:29:11 +0500

    ref: common/block

--- shared/components/common/block.tsx
+++ shared/components/common/block.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
 
-function Block({ children }: { children: any }) {
+function Block({ children, className }: { children: any, className?: string }) {
   return (
-    <div className='z-20 rounded-3xl p-6 shadow-lg dark:shadow-xl shadow-c_shadow dark:shadow-cd_shadow bg-c_bg-block dark:bg-cd_bg-block'>
+    <div className={`z-20 rounded-3xl p-6 shadow-lg dark:shadow-xl shadow-c_shadow dark:shadow-cd_shadow bg-c_bg-block dark:bg-cd_bg-block ${className}`}>
       {children}
     </div>
   );
