fdf28a0a3f70edcd2b1cc83ed74678b4e2dd09d5
esperor <pere_strelka@mail.ru>
2024-05-15 20:31:55 +0500

    style: center layout

--- shared/components/common/layout.tsx
+++ shared/components/common/layout.tsx
@@ -3,7 +3,7 @@ import React from "react";
 function Layout({ children }: { children: any }) {
   return (
     <div className="bg-c_bg dark:bg-cd_bg h-[90vh]">
-      <div className="container p-8 bg-c_bg dark:bg-cd_bg">{children}</div>
+      <div className="container p-8 bg-c_bg dark:bg-cd_bg mx-auto">{children}</div>
     </div>
   );
 }
