988feace7460d2916d42d0d9064386ce19c472c6
Todery <o.rodygina2003@gmail.com>
2024-05-17 15:58:01 +0500

    feat: event description

--- shared/components/event/sign_upCard.tsx
+++ shared/components/event/sign_upCard.tsx
@@ -2,8 +2,8 @@ import React from "react";
 
 function SignUpCard() {
   return (
-    <button className="bg-c_main dark:bg-cd_main rounded-3xl items-center justify-center flex h-12 w-full my-4">
-      <p className=" text-c_bg dark:text-cd_bg ">Записаться</p>
+    <button className="mt-7 bg-c_main dark:bg-cd_main rounded-3xl items-center justify-center flex h-12 w-full my-4">
+      <p className=" text-c_bg dark:text-cd_bg text-xs font-bold">Записаться</p>
     </button>
 );
 }
--- shared/components/event/view_the_mapCard.tsx
+++ shared/components/event/view_the_mapCard.tsx
@@ -2,8 +2,8 @@ import React from "react";
 
 function ViewTheMapCard() {
   return (
-    <button className="bg-gray-200 dark:bg-cd_main rounded-3xl dark:text-cd_bg items-center justify-center flex h-12 w-full ">
-      <p className=' text-neutral-700 dark:text-cd_bg '>Посмотреть на карте</p>
+    <button className="bg-gray-200 dark:bg-cd_bg rounded-3xl dark:text-cd_bg items-center justify-center flex h-12 w-full ">
+      <p className=' text-neutral-700 dark:text-c_bg text-xs font-bold'>Посмотреть на карте</p>
     </button>
   );
 }
