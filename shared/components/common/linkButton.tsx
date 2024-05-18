ac752a3c578e84a6269dff0497129f8b00ead076
ijo42 <admin@ijo42.ru>
2024-05-18 15:38:29 +0500

    refactor: event description eslint, rearrange

--- shared/components/common/linkButton.tsx
+++ shared/components/common/linkButton.tsx
@@ -1,31 +1,27 @@
-// @ts-ignore
 import React from "react";
 import LinkIcon from '~/../../shared/assets/link.svg?react';
 
 const LinkButton = (props: { link: any; }) => {
-  return (
-    <button
-      className="w-[53px] h-[53px] shrink-0 aspect-square"
-      onClick={() => {
-
-        window.open(props.link, '_blank');
-
-      }}
-
-    >
-
-      <div
-        className={` w-[40px] h-[40px] hover:scale-110`}
-      >
-        <div
-          className={`w-[40px] h-[40px] rounded-full  bg-amber-50`}
+    return (
+        <button
+            className="w-[53px] h-[53px] shrink-0 aspect-square"
+            onClick={() => {
+                window.open(props.link, '_blank');
+            }}
         >
-          <LinkIcon/>
-        </div>
-      </div>
 
-    </button>
-  )
+            <div
+                className={` w-[40px] h-[40px] hover:scale-110`}
+            >
+                <div
+                    className={`w-[40px] h-[40px] rounded-full  bg-amber-50`}
+                >
+                    <LinkIcon/>
+                </div>
+            </div>
+
+        </button>
+    )
 }
 
 
--- shared/components/event/viewMapCard.tsx
+++ shared/components/event/viewMapCard.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 
-function ViewTheMapCard() {
+function ViewMapCard() {
   return (
     <button className="bg-gray-200 dark:bg-cd_bg rounded-3xl dark:text-cd_bg items-center justify-center flex h-12 w-full ">
-      <p className=' text-neutral-700 dark:text-c_bg text-xs font-bold'>Посмотреть на карте</p>
+      <p className=" text-neutral-700 dark:text-c_bg text-xs font-bold">
+        Посмотреть на карте
+      </p>
     </button>
   );
 }
 
-export default ViewTheMapCard;
+export default ViewMapCard;
