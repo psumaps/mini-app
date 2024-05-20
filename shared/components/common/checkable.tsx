4010b13393bde0f36f3128edda0ee080899c0246
li3pm <118972242+li3pm@users.noreply.github.com>
2024-05-20 09:43:02 +0500

    feat: implement main controls (#14)
    button and checkable (radio and checkbox) controls
    Co-authored-by: esperor <pere_strelka@mail.ru>

---
+++ shared/components/common/checkable.tsx
@@ -0,0 +1,66 @@
+import React, { useMemo } from "react";
+
+export interface CheckableProps
+  extends React.InputHTMLAttributes<HTMLInputElement> {
+  label?: string;
+  classNameLabel?: string;
+}
+
+/**
+ * Alias для checkable с указанным типом radio.
+ */
+export const Radio = (props: CheckableProps) => (
+  <Checkable type="radio" {...props} />
+);
+/**
+ * Alias для checkable с указанным типом checkbox.
+ */
+export const Checkbox = (props: CheckableProps) => (
+  <Checkable type="checkbox" {...props} />
+);
+
+/**
+ * Компонент, имеющий два возможных состояния. Может функционировать как radio или как checkbox.
+ *
+ * @param {string} [props.name] - Атрибут name для элемента input.
+ * @param {string} [props.type="radio"] - Тип.
+ * @param {string} [props.id] - Атрибут id для элемента input. Если не указан, будет сгенерирован на основе типа и имени.
+ * @param {string} [props.label] - Текст метки для элемента input.
+ * @param {string} [props.classNameLabel="c3"] - Стили для элемента label.
+ * @param {React.InputHTMLAttributes<HTMLInputElement>} [props...] - Дополнительные параметры, которые будут распространены на элемент input.
+ */
+const Checkable = (props: CheckableProps) => {
+  const {
+    name,
+    type = "radio",
+    id = useMemo(
+      () => `${type}_${name}_${(Math.random() * 100).toFixed(0)}`,
+      [name]
+    ),
+    label,
+    classNameLabel = "c3",
+    ...rest
+  } = props;
+
+  return (
+    <div className="flex flex-row items-center gap-4">
+      <div className="relative size-3.5">
+        <input
+          type={type}
+          name={name}
+          id={id}
+          className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 z-[2] appearance-none size-5 p-0 focus:outline-none peer"
+          {...rest}
+        />
+        <div className="z-[1] rounded-full border-solid border border-c_accent absolute inset-0 bg-transparent peer-checked:after:scale-100 after:absolute after:top-1/2 after:left-1/2 after:-translate-x-1/2 after:-translate-y-1/2 after:size-2/3 after:rounded-full after:scale-0 after:transition-transform after:duration-200 after:ease-in-out after:bg-c_accent"></div>
+      </div>
+      {label && (
+        <label htmlFor={id} className={classNameLabel}>
+          {label}
+        </label>
+      )}
+    </div>
+  );
+};
+
+export default Checkable;
---
+++ shared/stories/checkable.stories.tsx
@@ -0,0 +1,72 @@
+import Checkable, {
+  CheckableProps,
+  Radio,
+  Checkbox
+} from "../components/common/checkable";
+import React from "react";
+import { StoryFn, Meta, StoryObj } from "@storybook/react";
+
+const meta = {
+  title: "Common/Checkable",
+  component: Checkable,
+  // @ts-expect-error subcomonents are supported badly
+  subcomponents: { Radio, Checkbox },
+  parameters: {
+    layout: "centered",
+  },
+  tags: ["autodocs"],
+  argTypes: {
+    type: {
+      options: ["radio", "checkbox"],
+      control: { type: "radio" },
+    },
+  },
+} satisfies Meta<typeof Checkable>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+const args: CheckableProps = {
+  name: "Checkable",
+  label: "Радио ",
+  type: "radio",
+};
+
+export const DefaultRadio: Story = {
+  render: (args) => <Radio {...args} />,
+  args: {
+    type: "radio",
+  },
+}
+
+export const DefaultCheckbox: Story = {
+  render: (args) => <Checkbox {...args} />,
+  args: {
+    type: "checkbox",
+  }
+}
+
+const PairTemplate: StoryFn<CheckableProps> = (args) => (
+  <div className="flex flex-col gap-4">
+    <Checkable {...args} />
+    <Checkable {...args} />
+  </div>
+);
+
+export const Pair = PairTemplate.bind({});
+Pair.args = args;
+
+const TwoPairsTemplate: StoryFn<CheckableProps> = (args) => {
+  const { name, label, ...rest } = args;
+  return (
+    <div className="grid grid-cols-2 grid-rows-2 gap-6">
+      <Checkable {...rest} name={`${name}_1`} label={`${label} 1`} />
+      <Checkable {...rest} name={`${name}_1`} label={`${label} 1`} />
+      <Checkable {...rest} name={`${name}_2`} label={`${label} 2`} />
+      <Checkable {...rest} name={`${name}_2`} label={`${label} 2`} />
+    </div>
+  );
+};
+
+export const TwoPairs = TwoPairsTemplate.bind({});
+TwoPairs.args = args;
