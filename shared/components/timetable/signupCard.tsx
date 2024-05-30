fe0095d992d976904cc3f1ad64049a2b40a630b4
Todery <o.rodygina2003@gmail.com>
2024-05-30 19:36:01 +0500

    refactor: EventDescription

--- shared/components/timetable/signupCard.tsx
+++ shared/components/timetable/signupCard.tsx
@@ -2,18 +2,16 @@ import React from "react";
 import Button from "../common/button";
 
 function SignUpCard(props: any) {
-    if (!props.link) return <div className="mt-7" />;
-    return  (
+    if (!props.link) return <div className="mt-7"/>;
+        return  (
             <Button className="mt-7 rounded-3xl items-center justify-center flex h-12 w-full my-4"
                     onClick={() =>
                         window.open(props.link, '_blank')
                     }
                     isContrast={true}>
-                <p className="text-c_bg dark:text-cd_bg text-xs font-bold">Записаться</p>
-
+                <div className="text-xs font-bold">Записаться</div>
             </Button>
         );
-    }
 }
 
 export default SignUpCard;
