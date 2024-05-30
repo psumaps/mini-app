b247c21a7a64dcb939a6ae4d542e6ae1f6ab9584
Todery <o.rodygina2003@gmail.com>
2024-05-30 22:38:50 +0500

    fix: SignUpCard

--- shared/components/timetable/signupCard.tsx
+++ shared/components/timetable/signupCard.tsx
@@ -5,11 +5,11 @@ const SignUpCard = ({ link }: { link?: string }) => {
   if (!link) return <div className="mt-7" />;
   return (
     <Button
-      className="mt-7 rounded-3xl items-center justify-center flex h-12 w-full my-4"
+      className="mt-7 rounded-3xl items-center justify-center flex h-12 w-full my-4 c3"
       onClick={() => window.open(link, '_blank')}
       isContrast
     >
-      <div className="text-xs font-bold">Записаться</div>
+      Записаться
     </Button>
   );
 };
