cd53aae0158caf30a47548555f8348816f3a12f0
Toderry <106995468+Toderry@users.noreply.github.com>
2024-05-30 18:59:13 +0500

    Update shared/components/event/signupCard.tsx
    Co-authored-by: esperor <49198951+esperor@users.noreply.github.com>

--- shared/components/timetable/signupCard.tsx
+++ shared/components/timetable/signupCard.tsx
@@ -3,16 +3,17 @@ import Button from "../common/button";
 
 function SignUpCard(props: any) {
     if (!props.link) return <div className="mt-7" />;
-        return  (
+    return  (
             <Button className="mt-7 rounded-3xl items-center justify-center flex h-12 w-full my-4"
                     onClick={() =>
                         window.open(props.link, '_blank')
                     }
                     isContrast={true}>
-                <div className="text-xs font-bold">Записаться</div>
+                <p className="text-c_bg dark:text-cd_bg text-xs font-bold">Записаться</p>
 
             </Button>
         );
+    }
 }
 
 export default SignUpCard;
