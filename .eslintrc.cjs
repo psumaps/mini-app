ca0a824f95cb991b344fb25e22d4413ecfb8f426
Aleksandr <admin@ijo42.ru>
2024-06-02 19:09:48 +0500

    ci(lint): ignore unsupported files
    Signed-off-by: Aleksandr <admin@ijo42.ru>

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -10,7 +10,8 @@ module.exports = {
     'plugin:@typescript-eslint/recommended-type-checked',
     'plugin:prettier/recommended',
   ],
-  ignorePatterns: ['dist', '.eslintrc.cjs', 'tailwind.config.ts', 'vite.config.ts', 'mapbox-gl-indoorequal', 'native'],
+  ignorePatterns: ['dist', '.eslintrc.cjs', 'tailwind.config.ts', 'vite.config.ts', 'mapbox-gl-indoorequal', 'native',
+    '**/*.svg', '**/*.json'],
   parser: '@typescript-eslint/parser',
   parserOptions: {
     ecmaVersion: 'latest',
@@ -19,7 +20,7 @@ module.exports = {
   },
   plugins: ['react', '@typescript-eslint', 'react-refresh', 'prettier'],
   rules: {
-    'no-void': "off",
+    'no-void': 'off',
     'class-methods-use-this': 'off',
     'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
     'import/no-extraneous-dependencies': [
@@ -29,25 +30,25 @@ module.exports = {
       },
     ],
     'import/extensions': ['off'],
-    "prettier/prettier": [
-      "error",
+    'prettier/prettier': [
+      'error',
       {
-        "endOfLine": "auto"
-      }
+        'endOfLine': 'auto',
+      },
     ],
     'func-style': ['error', 'declaration', { 'allowArrowFunctions': true }],
-    "no-underscore-dangle":  ["error", { "allowAfterThis": true }],
-    "no-plusplus": ["error", { "allowForLoopAfterthoughts": true }],
-    "react/jsx-props-no-spreading": "off",
-    "react/function-component-definition": ["error", {
-      "namedComponents": "arrow-function",
-      "unnamedComponents": "arrow-function"
+    'no-underscore-dangle': ['error', { 'allowAfterThis': true }],
+    'no-plusplus': ['error', { 'allowForLoopAfterthoughts': true }],
+    'react/jsx-props-no-spreading': 'off',
+    'react/function-component-definition': ['error', {
+      'namedComponents': 'arrow-function',
+      'unnamedComponents': 'arrow-function',
     }],
-    "react/require-default-props": "off",
-    "jsx-a11y/click-events-have-key-events": "off", // since the app is mobile-first
-    "jsx-a11y/no-noninteractive-element-interactions": "off",
-    "jsx-a11y/no-static-element-interactions": "off",
-    "no-use-before-define": "off", // bugs out
-    "@typescript-eslint/no-use-before-define": "off",
+    'react/require-default-props': 'off',
+    'jsx-a11y/click-events-have-key-events': 'off', // since the app is mobile-first
+    'jsx-a11y/no-noninteractive-element-interactions': 'off',
+    'jsx-a11y/no-static-element-interactions': 'off',
+    'no-use-before-define': 'off', // bugs out
+    '@typescript-eslint/no-use-before-define': 'off',
   },
 };
