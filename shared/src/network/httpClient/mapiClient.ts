e95bd77aba414c52e52450c891a302292e8663e5
esperor <pere_strelka@mail.ru>
2024-06-08 22:09:05 +0500

    fix: add axios types

--- shared/src/network/httpClient/mapiClient.ts
+++ shared/src/network/httpClient/mapiClient.ts
@@ -2,31 +2,27 @@ import axios from 'axios';
 import Poi from '../models/mapi/poi';
 import api from '../api';
 
-/* eslint-disable @typescript-eslint/no-unsafe-return */
-/* eslint-disable @typescript-eslint/no-unsafe-member-access */
-/* eslint-disable @typescript-eslint/no-unsafe-call */
-
 const client = {
-  getPoiById: async (type: string, id: string): Promise<Poi> => {
-    const response = await axios.get(`${api.mapi}/poi/${type}:${id}`);
+  getPoiById: async (type: string, id: string) => {
+    const response = await axios.get<Poi>(`${api.mapi}/poi/${type}:${id}`);
     return response.data;
   },
-  getAmenityList: async (): Promise<string[]> => {
-    const response = await axios.get(`${api.mapi}/amenity`);
-    return (response.data as string[]).filter(
+  getAmenityList: async () => {
+    const response = await axios.get<{ collection: string[] }>(
+      `${api.mapi}/amenity`,
+    );
+    return response.data.collection.filter(
       (item) => item !== null && item !== 'yes',
     );
   },
-  getPoiByAmenity: async (amenity: string): Promise<Poi[]> => {
-    const response = await axios.get(`${api.mapi}/amenity/${amenity}`);
+  getPoiByAmenity: async (amenity: string) => {
+    const response = await axios.get<{ collection: Poi[] }>(
+      `${api.mapi}/amenity/${amenity}`,
+    );
     return response.data.collection;
   },
-  search: async (
-    query: string,
-    limit: number = 10,
-    offset: number = 0,
-  ): Promise<Poi[]> => {
-    const response = await axios.get(
+  search: async (query: string, limit: number = 10, offset: number = 0) => {
+    const response = await axios.get<{ collection: Poi[] }>(
       `${api.mapi}/search/${query}?limit=${limit}&offset=${offset}`,
     );
     return response.data.collection;
