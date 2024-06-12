860125877605fcd22af590c60f6bfdaf5211025b
Todery <o.rodygina2003@gmail.com>
2024-06-12 17:14:30 +0500

    fix: button to bottom

--- shared/src/components/common/layout.tsx
+++ shared/src/components/common/layout.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Layout = ({ children }: { children: React.ReactNode }) => {
   return (
-    <div className="bg-c_bg dark:bg-cd_bg h-[90vh] relative px-4 -top-3">
+    <div className="bg-c_bg dark:bg-cd_bg h-[90vh] flex flex-col px-4 -top-3 pb-[4.5rem]">
       {children}
     </div>
   );
