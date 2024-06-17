3a2618e4f2135a4c282242d10bc9e1cbf03ecf8a
esperor <pere_strelka@mail.ru>
2024-06-17 14:39:30 +0500

    style: fix layout paddings & fix avatar svg

--- shared/src/components/common/block.tsx
+++ shared/src/components/common/block.tsx
@@ -6,7 +6,7 @@ const Block = (
   const { children, className, ...rest } = props;
   return (
     <div
-      className={`z-20 rounded-3xl p-6 shadow-lg dark:shadow-xl shadow-c_shadow dark:shadow-cd_shadow bg-c_bg-block dark:bg-cd_bg-block ${className}`}
+      className={`rounded-3xl p-4 shadow-lg dark:shadow-xl shadow-c_shadow dark:shadow-cd_shadow bg-c_bg-block dark:bg-cd_bg-block ${className}`}
       {...rest}
     >
       {children}
--- shared/src/components/common/layout.tsx
+++ shared/src/components/common/layout.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Layout = ({ children }: { children: React.ReactNode }) => {
   return (
     <div className="bg-c_bg dark:bg-cd_bg h-[90vh]">
-      <div className="container p-8 pt-0 bg-c_bg dark:bg-cd_bg mx-auto">
+      <div className="container px-[5dvw] bg-c_bg dark:bg-cd_bg mx-auto">
         {children}
       </div>
     </div>
