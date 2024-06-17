d964e764aa9c517251d23134d98b808e8047349f
esperor <pere_strelka@mail.ru>
2024-06-17 16:34:07 +0500

    style: calendar

--- shared/src/components/timetable/calendar/index.tsx
+++ shared/src/components/timetable/calendar/index.tsx
@@ -15,6 +15,7 @@ import {
 } from './calendarUtils';
 import { nodes, node } from '../../../utils/selector';
 import Button from '../../common/button';
+import Block from '../../common/block';
 import CalendarIcon from '../../../assets/calendar.svg?react';
 import ResetIcon from '../../../assets/reset.svg?react';
 import useAnimEnabled from '../../../hooks/useAnimEnabled';
@@ -164,63 +165,69 @@ const CustomCalendar = ({
   };
 
   return (
-    <div className={`flex flex-col ${className}`}>
-      <div className="fixed top-0 left-0 right-0 bottom-0 z-[-100]">
-        <div
-          id={divNowId}
-          className={`absolute border-2 border-solid border-c_accent rounded-full -translate-x-1/2 z-[-10] will-change-transform 
-            ${animEnabled && 'ease-in-out duration-300 transition-all'} 
-            ${isMinified ? 'h-20 -translate-y-5' : '-translate-y-1/2'} 
-            ${showNowDiv ? 'opacity-100' : 'opacity-0'}`}
-        />
-        <div
-          id={divActiveId}
-          className={`absolute border-2 border-solid border-c_accent bg-c_accent rounded-full -translate-x-1/2  z-[-10] will-change-transform 
-            ${animEnabled && 'ease-in-out duration-300 transition-all'} 
-            ${isMinified ? 'h-20 -translate-y-5' : '-translate-y-1/2'}`}
-        />
-      </div>
+    <div className={`${className}`}>
       <Button
         type="button"
         onClick={handleMinify}
-        className="bg-c_main dark:bg-cd_main size-10 rounded-full mx-auto shadow-xl mb-2"
+        className="bg-c_main dark:bg-cd_main size-10 rounded-full mx-auto shadow-xl -mb-6 z-10 relative"
         title="Свернуть календарь"
       >
         <CalendarIcon className="size-5 stroke-c_bg dark:stroke-cd_bg [stroke-width:0.005rem] fill-c_bg dark:fill-cd_bg" />
       </Button>
-      <div className="flex flex-row justify-between w-[90%] mx-auto mb-1 items-center">
-        <CalendarDropdown
-          date={
-            // возвращаем дату или первую дату периода
-            activeStartDate instanceof Date
-              ? activeStartDate
-              : activeStartDate[0]
-          }
-          onSelect={handleSelect}
-        />
-        <button
-          type="button"
-          className="flex-row flex gap-2 items-center"
-          onClick={handleReset}
-          title="Сбросить дату"
-        >
-          <h5 className="c3 text-c_inactive dark:text-cd_inactive">
-            {today.toLocaleDateString('ru-RU')}
-          </h5>
-          <ResetIcon
-            className={`w-5 h-5 fill-c_inactive dark:fill-cd_inactive ${animEnabled && 'transition-transform'} ${resetIconAnimation ? 'rotate-[360deg] duration-500' : 'duration-0'}`}
+      <Block
+        className={`flex flex-col relative pt-8 will-change-auto 
+          ${animEnabled && 'transition-all duration-300 ease-in-out'} 
+          ${isMinified ? 'pb-6' : ''}`}
+      >
+        <div className="fixed top-0 left-0 right-0">
+          <div
+            id={divNowId}
+            className={`absolute border-2 border-solid border-c_accent rounded-full -translate-x-1/2 will-change-transform
+              ${animEnabled && 'ease-in-out duration-300 transition-all'} 
+              ${isMinified ? '-translate-y-[1.15rem]' : '-translate-y-1/2'} 
+              ${showNowDiv ? 'opacity-100' : 'opacity-0'}`}
           />
-        </button>
-      </div>
-      <Calendar
-        className="text-center text-c_main dark:text-cd_main"
-        activeStartDate={activeStartDate}
-        value={value}
-        tileClassName={tileClassName}
-        onChange={handleChange}
-        showNavigation={false}
-        locale="ru-RU"
-      />
+          <div
+            id={divActiveId}
+            className={`absolute border-2 border-solid border-c_accent bg-c_accent rounded-full -translate-x-1/2 will-change-transform
+              ${animEnabled && 'ease-in-out duration-300 transition-all'} 
+              ${isMinified ? '-translate-y-[1.15rem]' : '-translate-y-1/2'}`}
+          />
+        </div>
+        <div className="flex flex-row justify-between w-[90%] mx-auto mb-1 items-center">
+          <CalendarDropdown
+            date={
+              // возвращаем дату или первую дату периода
+              activeStartDate instanceof Date
+                ? activeStartDate
+                : activeStartDate[0]
+            }
+            onSelect={handleSelect}
+          />
+          <button
+            type="button"
+            className="flex-row flex gap-2 items-center"
+            onClick={handleReset}
+            title="Сбросить дату"
+          >
+            <h5 className="c3 text-c_inactive dark:text-cd_inactive">
+              {today.toLocaleDateString('ru-RU')}
+            </h5>
+            <ResetIcon
+              className={`w-5 h-5 fill-c_inactive dark:fill-cd_inactive transition-transform ${resetIconAnimation ? 'rotate-[360deg] duration-500' : 'duration-0'}`}
+            />
+          </button>
+        </div>
+        <Calendar
+          className="text-center text-c_main dark:text-cd_main"
+          activeStartDate={activeStartDate}
+          value={value}
+          tileClassName={tileClassName}
+          onChange={handleChange}
+          showNavigation={false}
+          locale="ru-RU"
+        />
+      </Block>
     </div>
   );
 };
