0a388eaee11499cf554b31041da0a92667a0dddd
esperor <49198951+esperor@users.noreply.github.com>
2024-06-17 14:48:00 +0500

    style: switch vh -> dvh in layout

--- shared/src/components/common/layout.tsx
+++ shared/src/components/common/layout.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Layout = ({ children }: { children: React.ReactNode }) => {
   return (
-    <div className="bg-c_bg dark:bg-cd_bg h-[90vh]">
+    <div className="bg-c_bg dark:bg-cd_bg h-[90dvh]">
       <div className="container px-[5dvw] bg-c_bg dark:bg-cd_bg mx-auto">
         {children}
       </div>
