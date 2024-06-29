28d7f99d33a22adf966cec14aadd405c3165e25e
esperor <pere_strelka@mail.ru>
2024-06-29 13:44:26 +0500

    fix: create fallback for gradient

--- shared/src/components/timetable/eventListCard.tsx
+++ shared/src/components/timetable/eventListCard.tsx
@@ -16,6 +16,9 @@ const EventListCard = ({
   const { data } = usePalette(event.cover_image!, 2, 'rgbString', {
     crossOrigin: 'anonymous',
   });
+
+  const canDrawGradient = data?.[0] && data?.[1];
+
   return (
     <Block
       className="p-[0_!important] rounded-t-[2.5rem]"
@@ -32,30 +35,45 @@ const EventListCard = ({
       <div className="flex flex-col px-4 mt-3 pb-4">
         <div className="flex justify-between items-center">
           {event.tag && (
-            <div className="justify-center py-1.5 px-4 max-w-[60%] rounded-full border-2 border-cd_main ">
-              <p className="c3 text-center text-cd_main">{event.tag}</p>
+            <div
+              className={`justify-center py-1.5 px-4 max-w-[60%] rounded-full border-2 
+                ${canDrawGradient ? 'border-cd_main' : 'border-c_main dark:border-cd_main'}`}
+            >
+              <p
+                className={`c3 text-center ${canDrawGradient ? 'text-cd_main' : ''}`}
+              >
+                {event.tag}
+              </p>
             </div>
           )}
 
-          <p className="c3 text-cd_main">{date.toLocaleDateString('ru')}</p>
+          <p className={`c3 ${canDrawGradient ? 'text-cd_main' : ''}`}>
+            {date.toLocaleDateString('ru')}
+          </p>
         </div>
         <div className="flex justify-between mt-4">
           <div>
-            <h3 className="text-cd_main">{event.title}</h3>
-            <p className="mt-2 c2 text-cd_main">{event.location}</p>
+            <h3 className={canDrawGradient ? 'text-cd_main' : ''}>
+              {event.title}
+            </h3>
+            <p className={`mt-2 c2 ${canDrawGradient ? 'text-cd_main' : ''}`}>
+              {event.location}
+            </p>
           </div>
           <button
             className="pr-3 pl-10 scale-100 active:scale-90"
             type="button"
             onClick={() => onOpenDesc(`${event.id}`)}
           >
-            <RightArrowIcon className="fill-cd_main" />
+            <RightArrowIcon className={canDrawGradient ? 'fill-cd_main' : ''} />
           </button>
         </div>
 
         <div className="relative mt-6">
           {date < new Date() && (
-            <abbr className="absolute bottom-0 right-2 text-cd_main">
+            <abbr
+              className={`absolute bottom-0 right-2 ${canDrawGradient ? 'text-cd_main' : ''}`}
+            >
               Прошедшее
             </abbr>
           )}
