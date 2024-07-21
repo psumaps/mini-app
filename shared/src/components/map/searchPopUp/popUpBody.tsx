322ddc614032c92994d16032c987a52c569eacc6
esperor <pere_strelka@mail.ru>
2024-07-21 13:09:13 +0500

    feat&ref: replace poi name with poi info for closed state

--- shared/src/components/map/searchPopUp/popUpBody.tsx
+++ shared/src/components/map/searchPopUp/popUpBody.tsx
@@ -1,6 +1,5 @@
 import React, { forwardRef, useEffect, useState } from 'react';
 import Input from './input';
-import PoiInfo from './poiInfo';
 import PoiInfoDetails from './poiInfoDetails';
 import Search from './search';
 import useAnimEnabled from '../../../hooks/useAnimEnabled';
@@ -13,13 +12,11 @@ const PopUpBody = forwardRef(function PopUpBody(
     state,
     setState,
     selectedPoi,
-    setSelectedPoi,
     onSelect,
   }: {
     state: PopUpState;
     setState: React.Dispatch<React.SetStateAction<PopUpState>>;
     selectedPoi: Poi | null;
-    setSelectedPoi: React.Dispatch<React.SetStateAction<Poi | null>>;
     onSelect?: (poi: Poi) => void;
   },
   ref: React.ForwardedRef<HTMLInputElement>,
@@ -31,15 +28,6 @@ const PopUpBody = forwardRef(function PopUpBody(
   useEffect(() => {
     if (selectedPoi !== null) setSelectedPoiInner(selectedPoi);
   }, [selectedPoi]);
-
-  const handleClearPoi = (
-    e:
-      | React.MouseEvent<HTMLButtonElement, MouseEvent>
-      | React.TouchEvent<HTMLButtonElement>,
-  ) => {
-    e.stopPropagation();
-    setSelectedPoi(null);
-  };
 
   const inputStyles =
     (state === 'opened' || state === 'middle') && selectedPoi === null
@@ -70,26 +58,10 @@ const PopUpBody = forwardRef(function PopUpBody(
       />
       <div
         id={popUpBodyPoiContainerId}
-        className={`absolute -top-2 left-0 right-0 px-2 origin-bottom 
+        className={`absolute -top-2 left-0 right-0 px-2 origin-bottom pt-10
             ${animEnabled && 'transition-all duration-500 ease-in-out'}
             ${selectedPoi === null || state === 'closed' ? 'scale-y-0 opacity-0 h-0' : 'scale-y-100 opacity-100'}`}
       >
-        <PoiInfo
-          item={selectedPoiInner}
-          onClick={() => {}}
-          classNameInner=""
-          className="px-4"
-        />
-        <button
-          type="button"
-          onClick={handleClearPoi}
-          onTouchStartCapture={handleClearPoi}
-          className="absolute top-0 right-4 size-6 z-20"
-          title="Очистить выбранную точку"
-        >
-          <div className="w-full h-[1px] origin-center rotate-45 bg-c_secondary dark:bg-cd_secondary" />
-          <div className="w-full h-[1px] origin-center -rotate-45 bg-c_secondary dark:bg-cd_secondary" />
-        </button>
         <PoiInfoDetails item={selectedPoiInner} className="w-100 pt-3" />
       </div>
       <div
