9481295c2a4e78417aa2f0ab3aced6f7907d6e74
esperor <pere_strelka@mail.ru>
2024-08-22 18:01:36 +0500

    fix&ref: timetable card

--- shared/src/components/timetable/timetableCard/index.tsx
+++ shared/src/components/timetable/timetableCard/index.tsx
@@ -5,7 +5,7 @@ import Button from '../../common/button';
 
 interface Props {
   classData: Timetable.Class;
-  navigate?: ((s: string) => void);
+  navigate?: (s: string) => void;
 }
 const audRegex = /(\d{3}\/\d{1,2})/;
 
@@ -19,55 +19,44 @@ const TimetableCard = ({ classData, navigate }: Props) => {
     return classStatus(classData.date);
   }, [classData.date]);
 
-  const cardClassName = `${
-    isClassInProgress
-      ? 'bg-c_accent dark:bg-c_accent text-c_bg-block shadow-md'
-      : 'bg-cd_main shadow-md dark:bg-cd_bg-block'
-  }  w-full rounded-[2.5rem]`;
-
   const cardClassNameText = isClassInProgress ? 'text-c_bg-block' : '';
-
-  const renderContent = () => {
-    if (isError || !classData) {
-      return (
-        <div className="flex items-center justify-center h-full w-full">
-          <p className="text-center">Ошибка при чтении данных о занятии</p>
-        </div>
-      );
-    }
-
-    return (
-      <div className="grid gap-2 grid-cols-[80%_20%]">
-        <div>
-          <h3 className={`${cardClassNameText}  line-clamp-2 overflow-hidden`}>
-            {classData.discipline}
-          </h3>
-          <div className={`${cardClassNameText} c1 pb-1 pt-[0.6rem]`}>
-            {classData.teacher}
-          </div>
-          <div className={`${cardClassNameText} c2`}>{classData.place}</div>
-        </div>
-        <div>
-          <h3 className={`${cardClassNameText} text-center`}>
-            {classData.time}
-          </h3>
-          <h3 className={`${cardClassNameText} text-center `}>
-            {classData.type}
-          </h3>
-        </div>
-      </div>
-    );
-  };
 
   return (
     <Button
       onClick={() => {
         if (audStr) navigate?.(`/#q=${audStr}`);
       }}
-      className={`${cardClassName} text-start`}
+      className="text-start shadow-md w-full rounded-[2.5rem]"
+      variant={isClassInProgress ? 'accent' : 'primary'}
     >
-      <div className="py-7 pl-6 pr-8 flex justify-between items-start min-h-[120px]">
-        {renderContent()}
+      <div className="py-7 pl-6 pr-8 min-h-[120px] w-full">
+        {isError || !classData ? (
+          <div className="flex items-center justify-center h-full w-full">
+            <p className="text-center">Ошибка при чтении данных о занятии</p>
+          </div>
+        ) : (
+          <div className="justify-between flex-row flex">
+            <div className="mr-8">
+              <h3
+                className={`${cardClassNameText} line-clamp-2 overflow-hidden`}
+              >
+                {classData.discipline}
+              </h3>
+              <div className={`${cardClassNameText} c1 pb-1 pt-[0.6rem]`}>
+                {classData.teacher}
+              </div>
+              <div className={`${cardClassNameText} c2`}>{classData.place}</div>
+            </div>
+            <div>
+              <h3 className={`${cardClassNameText} text-center`}>
+                {classData.time}
+              </h3>
+              <h3 className={`${cardClassNameText} text-center `}>
+                {classData.type}
+              </h3>
+            </div>
+          </div>
+        )}
       </div>
     </Button>
   );
