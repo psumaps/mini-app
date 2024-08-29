d546c422b97a0c47b016e10f3b9d197e0ed8c6ba
Aleksandr <admin@ijo42.ru>
2024-08-29 17:03:44 +0500

    fix: scroll to top

--- shared/src/components/timetable/ScrollToTop.tsx
+++ shared/src/components/timetable/ScrollToTop.tsx
@@ -1,5 +1,3 @@
-'use client';
-
 import React, { useState, useEffect } from 'react';
 
 const UpArrowIcon = () => (
@@ -19,47 +17,36 @@ const UpArrowIcon = () => (
   </svg>
 );
 
-/*
-This React component creates a "scroll to top" button that becomes visible when the user scrolls down the page. 
-It includes a smooth scroll effect when clicked.
-*/
-
 const ScrollToTop = () => {
   // State to manage the visibility of the "scroll to top" button
-  const [visible, setVisible] = useState(false);
+  const [visible, setVisible] = useState(true); // todo: fix
 
-  // Function to scroll back to the top of the page smoothly
   const scrollToTop = () => {
-    window.scrollTo({
-      top: 0,
-      behavior: 'smooth',
-    });
+    document.getElementById('header')?.scrollIntoView({ behavior: 'smooth' });
   };
 
   // Add a scroll event listener to track when to show or hide the button
   useEffect(() => {
-    window.addEventListener('scroll', () => {
+    const listener = () => {
+      // ?: scrollY always is 0
       if (window.scrollY >= 200) {
         setVisible(true);
-      } else {
+      } /* else {
         setVisible(false);
-      }
-    });
-
-    // Cleanup: remove the event listener when the component is unmounted
-    return () => {
-      window.removeEventListener('scroll', () => {
-        // You can also remove the listener here for cleanup.
-      });
+      } */
     };
-  }, []); // An empty dependency array ensures that this effect runs only once when the component is mounted.
+    window.addEventListener('scroll', listener, true);
 
-  // Render the "scroll to top" button, which is visible or hidden based on the 'visible' state
+    return () => {
+      window.removeEventListener('scroll', listener, true);
+    };
+  }, []);
+
   return (
     <div>
       {visible && (
         <div
-          className={`fixed bottom-4 right-4 z-10 w-5 h-5 bg-white flex justify-center items-center p-4 rounded-lg bg-opacity-5 backdrop-blur-sm  ${
+          className={`fixed bottom-32 right-4 z-10 w-5 h-5 bg-white flex justify-center items-center p-4 rounded-lg bg-opacity-5 backdrop-blur-sm  ${
             visible ? 'visible' : 'hidden'
           } hover:pb-1`}
         >
