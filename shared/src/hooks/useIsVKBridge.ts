ae69180c0d8b5d1e1a5199bd489a0aac714792c5
esperor <pere_strelka@mail.ru>
2024-09-13 23:16:13 +0500

    fix: useIsVkBridge

--- shared/src/hooks/useIsVKBridge.ts
+++ shared/src/hooks/useIsVKBridge.ts
@@ -1,12 +1,16 @@
-import { useContext, useState } from 'react';
+import { useContext, useEffect, useState } from 'react';
 import { StorageContext, StorageType } from '../models/storage';
 
 const useVKBridge = () => {
   const storage = useContext(StorageContext);
   const [isVKBridge, setIsVKBridge] = useState(false);
-  void storage
-    ?.getStorageType()
-    .then((s) => setIsVKBridge(s === StorageType.vkbridge));
+
+  useEffect(() => {
+    void storage
+      ?.getStorageType()
+      .then((s) => setIsVKBridge(s === StorageType.vkbridge));
+  }, [storage]);
+
   return isVKBridge;
 };
 
