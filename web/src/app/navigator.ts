e248a91d99a17fbc9f0cbe1407a7d075416a5552
Aleksandr <admin@ijo42.ru>
2025-02-06 18:01:53 +0500

    chore: fallback history navigation on context provider

--- web/src/app/navigator.ts
+++ web/src/app/navigator.ts
@@ -2,18 +2,27 @@ import INavigator from 'psumaps-shared/src/models/navigator';
 import { NavigateFunction } from 'react-router-dom';
 
 class Navigator implements INavigator {
-  private readonly navigateFunction: NavigateFunction;
+  private readonly navigateFunction?: NavigateFunction;
 
-  constructor(navigateFunction: NavigateFunction) {
+  constructor(navigateFunction?: NavigateFunction) {
     this.navigateFunction = navigateFunction;
   }
 
   navigate(path: string): void {
-    this.navigateFunction(path);
+    if (this.navigateFunction) {
+      this.navigateFunction(path);
+    } else {
+      history.pushState({}, '', path);
+      history.go();
+    }
   }
 
   back(): void {
-    this.navigateFunction(-1);
+    if (this.navigateFunction) {
+      this.navigateFunction(-1);
+    } else {
+      history.back();
+    }
   }
 }
 
