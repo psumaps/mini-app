8236d9487d73db45239377f2f33cf9b305cca378
Aleksandr <admin@ijo42.ru>
2025-03-12 20:04:54 +0500

    chore: theme storage logic

--- web/src/widgets/navigationBar.tsx
+++ web/src/widgets/navigationBar.tsx
@@ -6,32 +6,19 @@ import getStoredTheme from 'psumaps-shared/src/utils/readTheme';
 import MapIcon from 'psumaps-shared/src/assets/map.svg?react';
 import TimetableIcon from 'psumaps-shared/src/assets/timetable.svg?react';
 import SettingsIcon from 'psumaps-shared/src/assets/settings.svg?react';
-import useTryQueryClient from 'psumaps-shared/src/hooks/useTryQueryClient';
-import { useQuery } from '@tanstack/react-query';
 import { StorageContext } from 'psumaps-shared/src/models/storage';
 import { NavigatorContext } from 'psumaps-shared/src/models/navigator';
 
 const NavigationBar = ({ className }: { className?: string }) => {
   const location = useLocation();
   const navigator = useContext(NavigatorContext);
-  const queryClient = useTryQueryClient();
   const storage = useContext(StorageContext);
 
-  const themeQuery = useQuery(
-    {
-      queryKey: ['storage', 'theme'],
-      queryFn: () => getStoredTheme(storage!),
-      refetchOnWindowFocus: false,
-      retry: false,
-      staleTime: Infinity,
-    },
-    queryClient,
-  );
-
   useEffect(() => {
-    if (themeQuery.data)
-      document.documentElement.classList.toggle('dark', themeQuery.data);
-  }, [themeQuery.data]);
+    void getStoredTheme(storage!).then((theme) => {
+      if (theme) document.documentElement.classList.toggle('dark', theme);
+    });
+  }, [storage]);
 
   // Определяем цвет иконки на основе текущего пути
   const fill = (path: string) => {
